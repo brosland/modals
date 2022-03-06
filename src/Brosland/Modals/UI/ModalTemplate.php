462780f87b5cab2514371ce593569c528edf5896
mbskot@gmail.com <mbskot@gmail.com>
2022-03-06 11:28:56 +0100

    Added ModalTemplate.

---
+++ src/Brosland/Modals/UI/ModalTemplate.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Brosland\Modals\UI;
+
+use Nette\Bridges\ApplicationLatte\Template;
+
+/**
+ * @property Modal $control
+ * @property string $modalTemplate
+ */
+class ModalTemplate extends Template
+{
+}
